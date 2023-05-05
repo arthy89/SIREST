8f53811a8c39df584ab6b429a36080e80fb35be0
Arhyel Ramos Flores <arthy89>
2023-05-04 23:44:53 -0500

    juntando3

--- resources/views/Backend/Categorias/categoriasindex.blade.php
+++ resources/views/Backend/Categorias/categoriasindex.blade.php
@@ -152,6 +152,10 @@
 
 
                         <p>AQUI SE MUESTRAN Las Categorias</p>
+
+                        @foreach ($categorias as $categoria)
+                            <img src="{{ asset($categoria->ruta) }}" alt="No imagen" width="150px">
+                        @endforeach
                     </div>
                 </div>
             </div>
